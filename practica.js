97f1bd50f2f3b7e2b7bde2146d6f6a8bd93144d8
gabriela restrepo <99907504+gabirestrepo1310@users.noreply.github.com>
2022-05-12 08:45:32 -0500

    trabajo1

--- practica.js
+++ practica.js
@@ -1,1 +1,17 @@
-
+let num1=parseInt(prompt("escribe un numero:"));
+let num2= parseInt (prompt("por favor escribe otro numero"));
+let operacion=prompt("escribe cual operacion deseas realizar entre los dos numeros que acabastes de escribir: ");
+let resultado = 0;
+if(operacion =="sumar") {
+    resultado = num1 + num2;
+}
+if(operacion == "restar") {
+    resusltado = num1 - num2;
+}
+if(operacion == "multiplicar") {
+    resultado = num1 * num2;
+}
+if(operacion == "dividir") {
+    resultado = num1 / num2;
+}
+alert("el resultado de tu operacion es:" + resultado);
